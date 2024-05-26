6a32495ca55d12e5e6a8f3cdbb4f20aaea21c55b
kamilrazadev <m.kamilrazabhatti@gmail.com>
2024-05-27 00:19:22 +0500

    first commit

---
+++ components/SpotlightShine.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SpotlightShine = () => {
+  return (
+    <div className='w-[140px] h-[400px] bg-my-blue-purple -rotate-45 absolute left-[25%] -top-28 blur-[50px] rounded-full'></div>
+  )
+}
+
+export default SpotlightShine
